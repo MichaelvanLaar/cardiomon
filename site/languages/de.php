5d0ff2155b03feae58a8fa13af79c691cd7d14a2
Michael van Laar <michael@van-laar.de>
2024-01-18 02:03:30 +0100

    🌐 i18n(ar.php, de.php, en.php): Update language files to include new translations for 'Carbon emission per year (in tons)', 'Company', and 'Country'. Also, change single quotes to double quotes for consistency.

--- site/languages/de.php
+++ site/languages/de.php
@@ -1,21 +1,26 @@
 <?php
 
 return [
-    'code' => 'de',
-    'default' => false,
-    'direction' => 'ltr',
-    'locale' => [
-        'LC_ALL' => 'de_DE'
+    "code" => "de",
+    "default" => false,
+    "direction" => "ltr",
+    "locale" => [
+        "LC_ALL" => "de_DE",
     ],
-    'name' => 'Deutsch',
-    'translations' => [
-        'Close menu' => 'Menü schließen',
-        'Open menu' => 'Menü öffnen',
-        'Switch to' => 'Wechseln zu',
-        'You are here' => 'Sie sind hier',
-        'Label for language switch' => 'DE',
-        'Language name for language switch' => 'Deutsch',
-        'Disclaimer for footer' => 'Diese Website ist eine <a href="https://github.com/MichaelvanLaar/cardiomon/wiki" target="_blank">Fallstudie</a> für den Informatikkurs „IPWA01-01“ an der <a href="https://www.iu.de/bachelor/informatik/fernstudium/" target="_blank">IU&nbsp;Internationale Hochschule</a>. Alle Inhalte und Daten sind frei erfunden.'
+    "name" => "Deutsch",
+    "translations" => [
+        "Carbon emission per year (in tons)" =>
+            "CO2-Emissionen pro Jahr (in Tonnen)",
+        "Close menu" => "Menü schließen",
+        "Company" => "Unternehmen",
+        "Country" => "Land",
+        "Disclaimer for footer" =>
+            'Diese Website ist eine <a href="https://github.com/MichaelvanLaar/cardiomon/wiki" target="_blank">Fallstudie</a> für den Informatikkurs „IPWA01-01“ an der <a href="https://www.iu.de/bachelor/informatik/fernstudium/" target="_blank">IU&nbsp;Internationale Hochschule</a>. Alle Inhalte und Daten sind frei erfunden.',
+        "Label for language switch" => "DE",
+        "Language name for language switch" => "Deutsch",
+        "Open menu" => "Menü öffnen",
+        "Switch to" => "Wechseln zu",
+        "You are here" => "Sie sind hier",
     ],
-    'url' => NULL
+    "url" => null,
 ];
--- site/languages/en.php
+++ site/languages/en.php
@@ -1,21 +1,26 @@
 <?php
 
 return [
-    'code' => 'en',
-    'default' => true,
-    'direction' => 'ltr',
-    'locale' => [
-        'LC_ALL' => 'en_US'
+    "code" => "en",
+    "default" => true,
+    "direction" => "ltr",
+    "locale" => [
+        "LC_ALL" => "en_US",
     ],
-    'name' => 'English',
-    'translations' => [
-        'Close menu' => 'Close menu',
-        'Open menu' => 'Open menu',
-        'Switch to' => 'Switch to',
-        'You are here' => 'You are here',
-        'Label for language switch' => 'EN',
-        'Language name for language switch' => 'English',
-        'Disclaimer for footer' => 'This website is a <a href="https://github.com/MichaelvanLaar/cardiomon/wiki" target="_blank">case study</a> for the computer science course “IPWA01-01” at <a href="https://www.iu.de/en/bachelor/informatik/distance-learning/" target="_blank">IU&nbsp;International University of Applied Sciences</a>. Contents and data are fictitious.'
+    "name" => "English",
+    "translations" => [
+        "Carbon emission per year (in tons)" =>
+            "Carbon emission per year (in tons)",
+        "Close menu" => "Close menu",
+        "Company" => "Company",
+        "Country" => "Country",
+        "Disclaimer for footer" =>
+            'This website is a <a href="https://github.com/MichaelvanLaar/cardiomon/wiki" target="_blank">case study</a> for the computer science course “IPWA01-01” at <a href="https://www.iu.de/en/bachelor/informatik/distance-learning/" target="_blank">IU&nbsp;International University of Applied Sciences</a>. Contents and data are fictitious.',
+        "Label for language switch" => "EN",
+        "Language name for language switch" => "English",
+        "Open menu" => "Open menu",
+        "Switch to" => "Switch to",
+        "You are here" => "You are here",
     ],
-    'url' => '/'
+    "url" => "/",
 ];
